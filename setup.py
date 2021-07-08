1b07dd69fb18ca0690a597cad66d2c95edab629a
mcopik <mcopik@gmail.com>
2021-07-08 16:04:01 +0200

    Update package name

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='faaskeeper-python',
+    name='faaskeeper',
     version='0.1.0-beta',
     install_requires=['boto3'],
-    url='https://github.com/spcl/serverless-zookeeper-client',
+    url='https://github.com/mcopik/faaskeeper-client',
     license='BSD-3',
     author='Marcin Copik',
     author_email='mcopik@gmail.com',
